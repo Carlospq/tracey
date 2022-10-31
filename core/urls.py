a07d081072c812acce2b89ba57db6bafaacb27fc
carlospq <carlospq88@gmail.com>
2022-10-31 16:43:23 +0100

    Added captcha to register view

--- core/urls.py
+++ core/urls.py
@@ -20,6 +20,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("apps.authentication.urls")), # Auth routes - login / register
 
+    path('captcha/', include('captcha.urls')),
+
     # ADD NEW Routes HERE
 
     # Leave `Home.Urls` as last the last line
