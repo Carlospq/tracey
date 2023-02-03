2ae72f6108c4cebad3ba05992d20e1319e9add6a
carlospq <carlospq88@gmail.com>
2023-02-03 11:00:55 +0100

    Rplot clades fixed

--- utils/ncbi_taxonomy/plotPhyloTree.R
+++ utils/ncbi_taxonomy/plotPhyloTree.R
@@ -1,10 +1,15 @@
-library("ape")
-library("ggplot2")
-library("ggtree")
+library("ape", quietly = TRUE)
+library("ggplot2", quietly = TRUE)
+library("ggtree", quietly = TRUE)
 
 nwk <- 'utils/ncbi_taxonomy/TRACEY_phylogeneticTree.newick'
 tree <- read.tree(file = nwk)
 groupInfo <- split(tree$tip.label, sapply(strsplit(tree$tip.label, "\\|"), "[", 2))
+groupInfo <- groupInfo[names(groupInfo) %in% "outer" == FALSE]
+#Add number of taxas to tag label
+for (i in 1:length(names(groupInfo)) ){
+  names(groupInfo)[[i]] <- paste0(names(groupInfo)[[i]], "\n", "(", length(groupInfo[[i]]), ")")
+}
 treeOTU <- groupOTU(tree, groupInfo)
 group_nodes <- lapply(groupInfo, function(x) { getMRCA(treeOTU, x) })
 
@@ -26,7 +31,7 @@ for (label in names(group_nodes)){
   if (label=="Unclassified"){next}
   tree_plot_wClades <- tree_plot_wClades + geom_cladelabel(node=group_nodes[label][[1]], 
                                                            label=label,
-                                                           fontsize = 10,
+                                                           fontsize = 6,
                                                            barsize=0.5,
                                                            offset=1, 
                                                            offset.text=4,
