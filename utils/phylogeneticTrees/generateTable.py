372594efeb50411f4638f07107fe469fde55ccd0
carlospq <carlospq88@gmail.com>
2023-02-07 10:36:22 +0100

    Tree plot tool

---
+++ utils/phylogeneticTrees/generateTable.py
@@ -0,0 +1,30 @@
+#python manage.py shell < utils/phylogeneticTrees/generateTable.py
+import pandas as pd
+from apps.home.models import *
+
+def get_parents(model, instance, instance_id, instance_parent_id, parents=[]):
+    if getattr(instance, instance_id) != getattr(instance, instance_parent_id): # if instance is not root...
+        parent = model.objects.get( **{ instance_id: getattr(instance, instance_parent_id) } )
+        parents.append( [getattr(instance, 'taxonomyrank'), getattr(instance, 'scientificname'), getattr(instance, instance_id)] )
+        get_parents(model, parent, instance_id, instance_parent_id, parents=parents)
+    else:
+        parents.append( [getattr(instance, 'taxonomyrank'), getattr(instance, 'scientificname'), getattr(instance, instance_id)] )
+        return parents
+    return parents
+
+table_dict = {}
+ranks = ['species group', 'species subgroup', 'cohort', 'family', 'tribe', 'class', 'biotype', 'genotype', 'section', 'order', 'superorder', 'superphylum', 'pathogroup', 'subphylum', 'parvorder', 'subfamily', 'species', 'isolate', 'infraorder', 'subkingdom', 'forma', 'phylum', 'strain', 'superkingdom', 'suborder', 'kingdom', 'infraclass', 'superclass', 'superfamily', 'subtribe', 'subcohort', 'forma specialis', 'serogroup', 'subclass', 'varietas', 'series', 'genus', 'serotype', 'subgenus', 'subspecies']
+
+for t in Taxonomies.objects.all():
+	table_dict[t.taxonomy_id] = []
+	parents = get_parents(Taxonomies, t, 'taxonomy_id', 'taxonomyparent_id', parents=[])
+	for r in ranks:
+		name = '-'
+		for x in parents:
+			if x[0] == r:
+			 	name = x[1]
+		table_dict[t.taxonomy_id].append(name)
+
+df = pd.DataFrame.from_dict(table_dict, orient='index')
+df.columns = ranks
+df.to_csv('utils/phylogeneticTrees/taxonomies.csv')
