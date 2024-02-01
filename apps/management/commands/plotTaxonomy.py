e4f999fc44b53acbc39eb243cfc0c04b99e56317
carlospq <carlospq88@gmail.com>
2024-02-01 17:38:34 +0100

    fixed plotTaxonomy.py

--- apps/management/commands/plotTaxonomy.py
+++ apps/management/commands/plotTaxonomy.py
@@ -4,7 +4,6 @@
 import sys
 import networkx as nx
 import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
 from apps.home.models import *
 
 from django.core.management.base import BaseCommand, CommandError
@@ -14,7 +13,9 @@ from django.contrib.auth.models import User
 
 class Command(BaseCommand):
 	def add_arguments(self, parser):
-		parser.add_argument('scientificname')
+		parser.add_argument("-sc", "--scientificname", type=str)
+		parser.add_argument("-du", "--depth_up", type=int, default=3)
+		parser.add_argument("-dd", "--depth_down", type=int, default=1)
 
 	def plot_component(self, g, scientificname): #dictionary = {"hg38": "lightgreen", "mm39": "lightblue", "danrer11": "yellow"}):
 		#org_color = dictionary
@@ -23,10 +24,11 @@ class Command(BaseCommand):
 		edges = g.edges()
 		edges_set = set(edges)
 		bidirectional = [True if (v[1],v[0]) in edges_set else False for v in edges]
-		edge_color = ['red' if x else 'gray' for x in bidirectional]
+		edge_color = ['red' if x else 'grey' for x in bidirectional]
 		# node_color = [org_color[x[0]] for x in nodes]
 		node_color = ["lightblue" if x[1]==scientificname else "lightgreen" for x in nodes ]
-		labels = {x:"_".join([x[1], str(x[3]), str(x[4])]) for x in nodes} ############### cambiar x[1] por tu_dictionary[x[1]]!
+		# labels = scientific_name, number_sequences, number_children, ncbi_ID
+		labels = {x:"_".join([x[1], str(x[3]), str(x[4]), str(x[0])]) for x in nodes} ############### cambiar x[1] por tu_dictionary[x[1]]!
 		pos=nx.spring_layout(g)
 		nx.draw_networkx_nodes(g,pos,nodelist=nodes,node_color=node_color)
 		nx.draw_networkx_edges(g,pos,edgelist=edges,edge_color=edge_color)
@@ -40,35 +42,35 @@ class Command(BaseCommand):
 		# plt.show()
 		plt.savefig("graph.pdf")
 
-	def add_taxonomies(self, taxonomy, ALL_NODES, EDGES, depth=0, depth_down=0):
-		depth += 1
-		if depth > 3 or depth_down > 1:
-			return
-		if taxonomy.taxonomyparent_id == 1:
-			return
-
+	def add_taxonomies(self, taxonomy, ALL_NODES, EDGES, depth_up=3, depth_up_counter=0, depth_down=1, depth_down_counter=0):
+		depth_up_counter += 1
 		_sequences = len(taxonomy.sequences_set.all())
 		_children = len(Taxonomies.objects.filter(taxonomyparent_id=taxonomy.taxonomy_id))
-		tax_node = (taxonomy.taxonomy_id, taxonomy.scientificname, taxonomy.taxonomyparent_id, _sequences, _children)
-		if tax_node in ALL_NODES:
-			return ALL_NODES, EDGES
+		_ncbiID = taxonomy.ncbi_taxonomy_id
+		tax_node = (taxonomy.taxonomy_id, taxonomy.scientificname, taxonomy.taxonomyparent_id, _sequences, _children, _ncbiID)
+		# if tax_node in ALL_NODES:
+		# 	return ALL_NODES, EDGES
 		ALL_NODES.append(tax_node)
 
 		parent_taxonomy = Taxonomies.objects.get(taxonomy_id=taxonomy.taxonomyparent_id)
 		_parent_sequences = len(parent_taxonomy.sequences_set.all())
 		_parent_children = len(Taxonomies.objects.filter(taxonomyparent_id=parent_taxonomy.taxonomy_id))
-		parent_node = (parent_taxonomy.taxonomy_id, parent_taxonomy.scientificname, parent_taxonomy.taxonomyparent_id, _parent_sequences, _parent_children)
+		_parent_ncbiID = parent_taxonomy.ncbi_taxonomy_id
+		parent_node = (parent_taxonomy.taxonomy_id, parent_taxonomy.scientificname, parent_taxonomy.taxonomyparent_id, _parent_sequences, _parent_children, _parent_ncbiID)
 
 		edge = (tax_node, parent_node)
 		if not edge in EDGES:
 			EDGES.append(edge)
 
-		self.add_taxonomies(parent_taxonomy, ALL_NODES, EDGES, depth=depth, depth_down=depth_down)
-		if len(Taxonomies.objects.filter(taxonomyparent_id=taxonomy.taxonomy_id)) > 1:
-			depth_down += 1
+		# Add next parent/child taxonomy if depth not reached
+		if depth_up_counter < depth_up:
+			self.add_taxonomies(parent_taxonomy, ALL_NODES, EDGES, depth_up=depth_up, depth_up_counter=depth_up_counter, depth_down=depth_down, depth_down_counter=depth_down_counter)
+
+		if len(Taxonomies.objects.filter(taxonomyparent_id=taxonomy.taxonomy_id)) >= 1 and depth_down_counter < depth_down:
+			depth_down_counter += 1
 			for t in Taxonomies.objects.filter(taxonomyparent_id=taxonomy.taxonomy_id):
-				self.add_taxonomies(t, ALL_NODES, EDGES, depth=1, depth_down=depth_down)
-			depth_down = 0
+				self.add_taxonomies(t, ALL_NODES, EDGES, depth_up=0, depth_up_counter=0, depth_down=depth_down, depth_down_counter=depth_down_counter)
+
 		return ALL_NODES, EDGES
 
 	def handle(self, *args, **options):
@@ -77,14 +79,17 @@ class Command(BaseCommand):
 		EDGES = []
 
 		taxonomies = Taxonomies.objects.filter(scientificname=options['scientificname'])
+		depth_up = options['depth_up']
+		depth_down = options['depth_down']
 		for t in taxonomies:
-			ALL_NODES, EDGES = self.add_taxonomies(t, ALL_NODES, EDGES, depth=0)
+			N, E = self.add_taxonomies(t, ALL_NODES, EDGES, depth_up=depth_up, depth_down=depth_down)
+			ALL_NODES += N
+			EDGES += E
 
 		G = nx.DiGraph()
 		G.add_nodes_from(ALL_NODES)
 		G.add_edges_from(EDGES, length = 10)
 		G_undirected = G.to_undirected()
-
 		components = sorted(list(G_undirected.subgraph(c) for c in nx.connected_components(G_undirected)),key=lambda x:-len(x))
 		Gplot = G.subgraph(components[0])
 
